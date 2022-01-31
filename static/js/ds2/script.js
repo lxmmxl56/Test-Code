506eab4935f7830a60159f91a28d628651fed88e
Matthew Long <matthew.long@t-mark.co.jp>
2022-01-31 12:05:14 +0900

    UPDATE: Add merge & quick sort and binary search

--- static/js/ds2/script.js
+++ static/js/ds2/script.js
@@ -1,32 +1,74 @@
 'use strict';
 
-import {bubbleSort, insertSort} from './sort.js';
+import {bubbleSort, insertSort, mergeSort, quickSort, binarySearch} from './sort.js';
 
-const unsorted = [6, 4, 3, 7, 5, 4, 8];
+let unsorted = [6, 4, 3, 7, 5, 4, 8];
 let sorted = [];
 let t0 = 0;
 let t1 = 0;
 
 console.log('~~~~~~~~~~~~~~~~~~~~~~~~')
 console.log('- BUBBLE -')
-console.log('~~~~~~~~~~~~~~~~~~~~~~~~') 
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~')
 console.log(unsorted);
-t0 = performance.now();
-for (let i = 0; i < 1000000; i++) {
+// t0 = performance.now();
+// for (let i = 0; i < 1000000; i++) {
     sorted = bubbleSort(unsorted);
-}
-t1 = performance.now();
-console.log(t1 - t0);
+// }
+// t1 = performance.now();
+// console.log(t1 - t0);
 console.log(sorted);
+
+unsorted = [6, 4, 3, 7, 5, 4, 8];
+sorted = [];
 
 console.log('~~~~~~~~~~~~~~~~~~~~~~~~')
 console.log('- Insert -')
 console.log('~~~~~~~~~~~~~~~~~~~~~~~~')
 console.log(unsorted);
-t0 = performance.now();
-for (let i = 0; i < 1000000; i++) {
+// t0 = performance.now();
+// for (let i = 0; i < 1000000; i++) {
     sorted = insertSort(unsorted);
-}
-t1 = performance.now();
-console.log(t1 - t0);
+// }
+// t1 = performance.now();
+// console.log(t1 - t0);
 console.log(sorted);
+
+unsorted = [6, 4, 3, 7, 5, 4, 8];
+sorted = [];
+
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~')
+console.log('- Merge -')
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~')
+console.log(unsorted);
+// t0 = performance.now();
+// for (let i = 0; i < 1000000; i++) {
+    sorted = mergeSort(unsorted);
+// }
+// t1 = performance.now();
+// console.log(t1 - t0);
+console.log(sorted);
+
+unsorted = [6, 4, 3, 7, 5, 4, 8];
+sorted = [];
+
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~')
+console.log('- Quick -')
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~')
+console.log(unsorted);
+// t0 = performance.now();
+// for (let i = 0; i < 1000000; i++) {
+    sorted = quickSort(unsorted);
+// }
+// t1 = performance.now();
+// console.log(t1 - t0);
+console.log(sorted);
+
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~')
+console.log('- Binary Search -')
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~')
+console.log(sorted);
+console.log(binarySearch(sorted, 3));
+console.log(binarySearch(sorted, 5));
+console.log(binarySearch(sorted, 8));
+console.log(binarySearch(sorted, 0));
--- static/js/ds2/sort.js
+++ static/js/ds2/sort.js
@@ -35,3 +35,81 @@ export function insertSort(toSort) {
     }
     return sorted;
 }
+
+function merge(left, right) {
+    const sorted = [];
+    while(left.length && right.length) {
+        if (left[0] < right[0]) {
+            sorted.push(left.shift());
+        } else {
+            sorted.push(right.shift());
+        }
+    }
+    return [...sorted, ...left, ...right];
+}
+
+export function mergeSort(toSort) {
+    const sorted = [...toSort];
+    const half = sorted.length/2;
+    if (sorted.length <= 1) {
+        return sorted;
+    }
+    const left = sorted.splice(0, half);
+    return merge(mergeSort(left), mergeSort(sorted));
+}
+
+function pivot(value, left, right) {
+    const sorted = [value];
+    while (left.length) {
+        if (left[0] < value) {
+            sorted.unshift(left.shift());
+        } else {
+            sorted.push(left.shift());
+        }
+    }
+    while (right.length) {
+        if (right[0] < value) {
+            sorted.unshift(right.shift());
+        } else {
+            sorted.push(right.shift());
+        }
+    }
+}
+
+export function quickSort(toSort) {
+    if (toSort.length <= 1) {
+        return toSort;
+    }
+    const pivotIndex = Math.ceil(toSort.length/2)
+    const pivot = toSort[pivotIndex];
+
+    const left = [];
+    const right = [];
+
+    for (let i = 0; i < toSort.length; i++) {
+        if (pivotIndex == i) {
+            continue;
+        }
+        toSort[i] < pivot ? left.push(toSort[i]) : right.push(toSort[i]);
+    }
+
+    return quickSort(left).concat(pivot, quickSort(right));
+}
+
+export function binarySearch(array, value) {
+    let start = 0;
+    let end = array.length - 1;
+
+    while (start <= end) {
+        const middle = Math.ceil((start + end) / 2);
+
+        if (value == array[middle]) {
+            return middle;
+        } else if (array[middle] < value) {
+            start = middle + 1;
+        } else {
+            end = middle - 1;
+        }
+    }
+    return false
+}

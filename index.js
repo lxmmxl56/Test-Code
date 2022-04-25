e7b670c9cd326aac592c245a183b1ddc382d0cad
play.js <git@gituser.com>
2022-04-26 06:04:06 +0900

    Add math dependency and bigo n^2 example

--- index.js
+++ index.js
@@ -25,3 +25,13 @@ console.log(myBigO.pairSum(2));
 console.log(myBigO.pairSum(3));
 console.log(myBigO.pairSum(4));
 console.log(myBigO.pairSum(5));
+
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~');
+console.log('- Print Pairs -');
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~');
+console.log(myBigO.printPairs('1'));
+console.log(myBigO.printPairs('12'));
+console.log(myBigO.printPairs('123'));
+console.log(myBigO.printPairs('1234'));
+console.log(myBigO.printPairs('12345'));
+
--- static/js/crack/bigo.js
+++ static/js/crack/bigo.js
@@ -1,7 +1,9 @@
 'use strict';
+  
+const math = require('mathjs');
 
 module.exports = class BigO {
-
+  
   sum(n) {
     if (n <= 0) {
       return 0;
@@ -19,5 +21,18 @@ module.exports = class BigO {
 
   pairSumRec(a, b) {
     return a + b;
+  }
+  
+  printPairs(a) {
+    const n = a.length;
+    let count = 0;
+    for (let i = 0; i < n; i++) {
+      for (let j = 0; j < n; j++) {
+        count++;
+        console.log(`${i}:${j}`);
+      }
+    }
+    console.log(`Length: ${n}, Count: ${count}`)
+    return (math.sqrt(count) === n);
   }
 }

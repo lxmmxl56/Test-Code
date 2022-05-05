720a8df9cac4196a2269f2f4d2b840ae3e6db078
Matthew N. Long <lxmmxl56@gmail.com>
2022-05-06 08:29:15 +0900

    UPDATE: Tech working

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const Tech = require('./static/js/crack/tech.js');
+const Tech = require('./tech.js');
 
 const myTech = new Tech();
 
@@ -8,3 +8,6 @@ console.log('~~~~~~~~~~~~~~~~~~~~~~~~');
 console.log('- Square Root -');
 console.log('~~~~~~~~~~~~~~~~~~~~~~~~');
 console.log('0:', myTech.x(0));
+console.log('1:', myTech.x(1));
+console.log('2:', myTech.x(2));
+console.log('3:', myTech.x(3));

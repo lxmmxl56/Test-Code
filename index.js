d1ddbb80e74df0512cdb9e0f55989e1254ad8af0
Matt Long <lxmmxl56@gmail.com>
2022-05-06 22:42:05 +0900

    MINOR

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Tech = require('./static/js/crack/tech.js');
 const myTech = new Tech();
 
 console.log('~~~~~~~~~~~~~~~~~~~~~~~~');
-console.log('- Square Root -');
+console.log('- x -');
 console.log('~~~~~~~~~~~~~~~~~~~~~~~~');
 console.log('0:', myTech.x(0));
 console.log('1:', myTech.x(1));

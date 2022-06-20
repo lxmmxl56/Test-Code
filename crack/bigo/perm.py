7300d255063f22e96547bb9c7b7b0016b7423baf
Matthew Long <lxmmxl56@gmail.com>
2022-06-21 05:51:06 +0900

    UPDATE: Python permute

--- crack/bigo/perm.py
+++ crack/bigo/perm.py
@@ -1,38 +1,27 @@
 import sys
 
 def main(n):
+    res = permute(n)
+    print(res)
+
+def permute(n):
+    if len(n) <= 1:
+        return [n]
     chars = list(n)
     sol = []
-    print(chars)
-    for char in chars:
-        print('char', char)
+    for idx, char in enumerate(chars):
         others = []
         for i in range(0, len(n)):
             other = n[i]
-            if char == other:
-                print('skip', char)
+            if idx == i:
                 continue
             else:
                 others.append(other)
-        print('others', others)
-        # newSol = sol.copy()
-        # for perm in sol:
-        #     print(perm)
-        #     newSol.remove(perm)
-        #     res = perm
-        #     for i in range(0, len(perm) + 1):
-        #         res = res[:i] + char + res[i:]
-        #         print(res)
-        #         newSol.add(res)
-        #     sol = newSol
-        #     print(sol)
-    print(sol)
-
-def permute(n, char):
-    res = n
-    for i in range(0, len(n) + 1):
-        res = res[:i] + char + res[i:]
-    return res
+        others = ''.join(others)
+        permutations = permute(others)
+        for perm in permutations:
+            sol.append(char + perm)
+    return sol
 
 if __name__ == '__main__':
     n = sys.argv[1]
